b593be4efb0da3d692812d4aed5ca16428e14a78
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-10 16:16:29 +0600

    Starter templete updated

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,26 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+import TopLeftImg from "./TopLeftImg";
+import Header from "./Header";
+import Nav from "./Nav";
+
+//font settings
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+//components
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,46 @@
-//  links
-const links = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+import {
+  HiHome,
+  HiUser,
+  HiRectangleGroup,
+  HiViewColumns,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi2";
+
+import Link from 'next/link';
+import { useRouter } from "next/navigation";
+
+export const navData = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup  /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav>
+    <div>
+      {navData.map((link, index) => (
+        <Link key={index} href={link.path}>
+          {link.icon}
+        </Link>
+      ))}
+    </div>
+    </nav>
+  );
 };
 
 export default Nav;
