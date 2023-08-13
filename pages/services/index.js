754c25289f6434cf11e62e1136b73a29d308c2d4
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-14 01:01:59 +0600

    Service page updated

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,31 @@
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+import ServiceSlider from "../../components/ServiceSlider";
+
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+       <div className="flex flex-col xl:flex-row gap-x-8">
+        {/* text */}
+        <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+        <h2 className="h2 xl:mt-8">My services <span className="text-sky-700">.</span>
+      </h2>
+      <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+        This is my Service page I will Add mY services here and I will write the appropeate text after complete the project
+
+      </p>
+        </div>
+        {/* slider */}
+        <ServiceSlider />
+
+       </div>
+      </div>
+      <Bulb />
+    </div>
+  )
 };
 
 export default Services;
