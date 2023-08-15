c094a58f135b4085113d576b67fee1370e3cee73
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-15 15:47:42 +0600

    Testimonials page added

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,3 +1,15 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { FaQuoteLeft } from "react-icons/fa";
+import Image from "next/image";
+
+
+
 // testimonial data
 const testimonialData = [
   {
@@ -23,8 +35,53 @@ const testimonialData = [
   },
 ];
 
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+    navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className="h-[440px] "
+    >
+      {testimonialData.map((feedBack, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16 ">
+              {/* avater, name, position */}
+              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div className="flex flex-col justify-center text-center">
+                  {/* avater */}
+                  <div>
+                    <Image src={feedBack.image} alt="feedback giver Image" width={110} height={110} />
+                  </div>
+                  {/* name */}
+                  <div className="text-lg">{feedBack.name}</div>
+                  {/* position */}
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">{feedBack.position}</div>
+                </div>
+              </div>
+
+              {/* quote and message */}
+              <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+             <div className="mb-4">
+              {/* icon */}
+              <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0 "/>
+
+             </div>
+             <div className="xl:text-lg text-center md:text-left mb-4">
+             {feedBack.message}
+             </div>
+              </div>
+
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
